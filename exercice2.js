bb936bb084964fd60986d46866bee812646b0cbc
Margot Herail <margot.herail@hotmail.fr>
2023-09-07 09:52:05 +0200

    exercice2

---
+++ exercice2.js
@@ -0,0 +1,11 @@
+// Addition
+var resultatAddition = 5 + 3;
+console.log("Addition : " + resultatAddition);
+
+// Soustraction
+var resultatSoustraction = 10 - 4;
+console.log("Soustraction : " + resultatSoustraction);
+
+// Multiplication
+var resultatMultiplication = 6 * 2;
+console.log("Multiplication : " + resultatMultiplication);
