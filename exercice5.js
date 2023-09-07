2eb81fec1e58e75c4ae332fa0451613f70e1f022
Margot Herail <margot.herail@hotmail.fr>
2023-09-07 10:23:33 +0200

    exercice 4 et 5

---
+++ exercice5.js
@@ -0,0 +1,7 @@
+// Création d'un tableau
+var fruits = ["Pomme", "Banane", "Orange", "Fraise", "Cerise"];
+
+// Utilisation d'une boucle for pour afficher chaque nom de fruit
+for (var i = 0; i < fruits.length; i++) {
+  console.log(fruits[i]);
+}
