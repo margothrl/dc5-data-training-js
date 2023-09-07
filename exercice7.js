9420b606347a7655b01e54c4f0eb752f8dd99a94
Margot Herail <margot.herail@hotmail.fr>
2023-09-07 10:32:36 +0200

    exercice7

---
+++ exercice7.js
@@ -0,0 +1,12 @@
+// Création de l'objet personne
+var personne = {
+    nom: "Margot Herail",
+    age: 23,
+    ville: "Montpellier"
+  };
+  
+  // Affichage des propriétés de l'objet dans la console
+  console.log("Nom : " + personne.nom);
+  console.log("Âge : " + personne.age);
+  console.log("Ville : " + personne.ville);
+  
