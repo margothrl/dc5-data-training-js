3e11408e86e66e7a994b2d5c0b0e2f5ec59922ab
Margot Herail <margot.herail@hotmail.fr>
2023-09-07 11:12:55 +0200

    exercice1 partie 2

---
+++ 2.Mobilisation articulaire/exercice1.js
@@ -0,0 +1,9 @@
+function additionner(nombre1, nombre2) {
+    var somme = nombre1 + nombre2;
+    return somme;
+  }
+  
+  // Exemple d'utilisation de la fonction
+  var resultat = additionner(5, 3);
+  console.log("La somme est : " + resultat);
+  
