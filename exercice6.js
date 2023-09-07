58b03f4940c6d4c4c00b2e7b17f532b0714e4471
Margot Herail <margot.herail@hotmail.fr>
2023-09-07 10:29:06 +0200

    exercice6

---
+++ exercice6.js
@@ -0,0 +1,9 @@
+// Fonction calculerSomme avec deux paramètres, la variable somme permet de faire l'addition, return permet de donner le résulat comme valeur à la fonction :
+function calculerSomme(nombre1, nombre2) {
+  var somme = nombre1 + nombre2;
+  return somme;
+}
+
+// Appel de la fonction + exemples de nombres
+var resultat = calculerSomme(5, 3);
+console.log("La somme est : " + resultat);
